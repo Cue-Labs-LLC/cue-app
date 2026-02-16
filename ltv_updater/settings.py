b116fdb5a24930a8401084cabe2e59f22faa874a
obarton <owen17101@msn.com>
2026-02-15 18:55:44 -0800

    reduce redis timeout

--- ltv_updater/settings.py
+++ ltv_updater/settings.py
@@ -253,8 +253,8 @@ if _CELERY_BROKER_URL:
             'LOCATION': _cache_url,
             'TIMEOUT': 300,
             'OPTIONS': {
-                'socket_connect_timeout': 2,
-                'socket_timeout': 2,
+                'socket_connect_timeout': 0.5,
+                'socket_timeout': 0.5,
             }
         }
     }
